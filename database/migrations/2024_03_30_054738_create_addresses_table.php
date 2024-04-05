77707574ce3e3ba83abf3a6b80c27e2f2de0a8e9
yanuarfauzan <yanuarisnain017@gmail.com>
2024-04-05 10:46:48 +0700

    menambahkan fitur user tambah beberapa alamat

--- database/migrations/2024_03_30_054738_create_addresses_table.php
+++ database/migrations/2024_03_30_054738_create_addresses_table.php
@@ -13,12 +13,12 @@ return new class extends Migration {
         Schema::create('address', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->uuid('user_id');
-            $table->string('unit_number')->nullable();
+            $table->string('detail')->nullable();
             $table->string('postal_code')->nullable();
-            $table->string('street_name')->nullable();
+            $table->string('address')->nullable();
             $table->string('city')->nullable();
             $table->string('province')->nullable();
-            $table->boolean('is_verified')->nullable();
+            $table->boolean('is_default')->nullable();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
         });
--- database/migrations/2024_04_02_174947_create_sessions_table.php
+++ database/migrations/2024_04_02_174947_create_sessions_table.php
@@ -12,12 +12,13 @@ return new class extends Migration {
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->uuid('user_id');
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->text('payload');
             $table->integer('last_activity');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
         });
     }
 
