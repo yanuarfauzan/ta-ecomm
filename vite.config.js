836797489c0a5c6164d4c9aa53736939e81cbec2
yanuarfauzan <yanuarisnain017@gmail.com>
2024-05-16 14:58:11 +0700

    "Removed debug code, refactored `isVariationDifferent` method, and updated Vite configuration"

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
+        laravel({
+            input: [
+                "resources/js/app.js",
+                "resources/css/app.css",
+            ],
+            refresh: true
         }),
     ],
 });
